df1ff3725fa237a4f568d80b0a91bde10401ff77
qiujiayu <jiayu.qiu@joy2u.cn>
2016-05-03 19:28:42 +0800

    fix bug

--- src/test/java/com/test/AutoLoadCacheService.java
+++ src/test/java/com/test/AutoLoadCacheService.java
@@ -7,9 +7,13 @@ import com.jarvis.cache.annotation.Cache;
 @IocBean
 public class AutoLoadCacheService {
 
-	@Cache(autoload = true, expire = 120, key = "'test_'+#hash(#args[0])")
+	@Cache(autoload = true, expire = 125, key = "'test_'+#hash(#args[0])")
 	public AutoLoadCache getData(AutoLoadCache data) {
 		System.out.println("aa >" + data.getName());
-		return data;
+		AutoLoadCache tmp=new AutoLoadCache();
+		tmp.setId(data.getId());
+		tmp.setAge(data.getAge());
+		tmp.setName(data.getName());
+		return tmp;
 	}
 }
--- src/test/java/com/test/SimpleAopConfigureTest.java
+++ src/test/java/com/test/SimpleAopConfigureTest.java
@@ -1,26 +1,35 @@
 package com.test;
 
-import org.nutz.dao.Dao;
 import org.nutz.ioc.Ioc;
 import org.nutz.ioc.impl.NutIoc;
 import org.nutz.ioc.loader.combo.ComboIocLoader;
 import org.nutz.json.Json;
 
 public class SimpleAopConfigureTest {
-
+    private static Ioc ioc;
 	public static void main(String[] args) throws ClassNotFoundException {
-		Ioc ioc = new NutIoc(new ComboIocLoader("*json", "/ioc", "*anno", "com.test", "*com.jarvis.cache.aop.nutz.AutoLoadCacheIocLoader"));
-		AutoLoadCacheService autoLoadCacheService = ioc.get(AutoLoadCacheService.class);
-		AutoLoadCache data = new AutoLoadCache();
-		data.setId(1);
-		data.setAge(20);
-		data.setName("test1");
-		AutoLoadCache res1 = autoLoadCacheService.getData(data);
-		data.setAge(30);
-		System.out.println(Json.toJson(res1));
-		data.setName("test2");
-		AutoLoadCache res2 = autoLoadCacheService.getData(data);
-		System.out.println(Json.toJson(res2));
+		ioc = new NutIoc(new ComboIocLoader("*json", "/ioc", "*anno", "com.test", "*com.jarvis.cache.aop.nutz.AutoLoadCacheIocLoader"));
+		test();
+		try {
+            Thread.sleep(120*1000);
+        } catch(InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+		test();
 		ioc.depose();
+	}
+	private static void test(){
+	    AutoLoadCacheService autoLoadCacheService = ioc.get(AutoLoadCacheService.class);
+        AutoLoadCache data = new AutoLoadCache();
+        data.setId(1);
+        data.setAge(20);
+        data.setName("test1");
+        AutoLoadCache res1 = autoLoadCacheService.getData(data);
+        System.out.println("res1=="+Json.toJson(res1));
+        data.setAge(30);
+        data.setName("test2");
+        AutoLoadCache res2 = autoLoadCacheService.getData(data);
+        System.out.println("res2=="+Json.toJson(res2));
 	}
 }
